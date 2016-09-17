41a74277f431f92672159979398afcc5ecc76982
Chase Drum <chasedrum@mac.com>
2016-09-16 21:09:46 -0700

    Initial Commit

---
+++ Devslopes-StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Devslopes-StackViewDemo
+//
+//  Created by Chase Drum on 9/16/16.
+//  Copyright © 2016 Dawghaus, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
